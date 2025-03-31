72131af810318d6710580a3b5e95e4a59517c264
Gary Jauffret <garyjauffret@gmail.com>
2025-03-31 18:57:15 +0200

    first version to work

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -15,8 +15,6 @@ c.Authenticator.admin_users = ["user1"]
 
 # Suppress the warning about no allow config
 c.Authenticator.any_allow_config = True
-# Alternative: allow any user who can authenticate
-# c.Authenticator.allow_all = True
 
 # Admin access
 c.JupyterHub.admin_access = True
@@ -28,21 +26,29 @@ c.JupyterHub.api_tokens = {
 }
 
 # Docker spawner config
+import os
 from dockerspawner import DockerSpawner
 c.JupyterHub.spawner_class = DockerSpawner
+
+# Get the network name from environment variable or use default
+network_name = os.environ.get("DOCKER_NETWORK_NAME", "jupyterhub-network")
 
 c.DockerSpawner.image = "jupyter-lab"
 c.DockerSpawner.remove_containers = True
 c.DockerSpawner.volumes = {
-    'jupyterhub-user-{username}': '/home/jovyan/work'
+    'jupyterhub-user-{username}': '/home/jovyan/work',
+    os.path.join(os.getcwd(), 'notebooks'): '/home/jovyan/work/notebooks'
 }
-c.DockerSpawner.network_name = "jupyterhub-network"
-c.DockerSpawner.debug = True
 
-# Explicitly set Docker client args
-c.DockerSpawner.use_internal_ip = True
+# Container name pattern - fix the formatting
+c.DockerSpawner.name_template = "jupyter-{username}"
+
+# IMPORTANT: Use the correct network name here
+c.DockerSpawner.network_name = network_name
+
+# Use the Docker host's network - also update the network mode here
 c.DockerSpawner.extra_host_config = {
-    'network_mode': 'jupyterhub-network'
+    'network_mode': network_name
 }
 
 # Environment variables for spawned containers
@@ -57,3 +63,23 @@ c.Spawner.default_url = '/lab'
 
 # Debug logging
 c.JupyterHub.log_level = 'DEBUG'
+
+# Ensure data directory exists
+data_dir = "/data"
+os.makedirs(data_dir, exist_ok=True)
+
+# Store data in the mounted volume
+c.JupyterHub.cookie_secret_file = os.path.join(data_dir, 'jupyterhub_cookie_secret')
+c.JupyterHub.db_url = 'sqlite:///' + os.path.join(data_dir, 'jupyterhub.sqlite')
+
+# Configuration pour le proxy et routage des requêtes
+# IMPORTANT: Changed this from the literal {username} to use string formatting
+c.DockerSpawner.prefix = '/user/{username}'  # This is correct, DockerSpawner will replace {username}
+
+c.JupyterHub.hub_connect_ip = 'jupyterhub'
+c.ConfigurableHTTPProxy.api_url = 'http://jupyterhub:8001'
+c.ConfigurableHTTPProxy.should_start = True
+
+# Important: assurez-vous que ces lignes sont activées
+c.Spawner.default_url = '/lab'
+c.Spawner.cmd = ['jupyter-labhub']
